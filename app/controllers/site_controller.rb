a9229c1200ce48ff156a89ed00214c66e983519f
Kamil Przybylski <supermazena@gmail.com>
2015-05-19 14:12:00 +0200

    First content (index, about, help, doors)

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,13 @@
+class SiteController < ApplicationController
+  def index
+  end
+
+  def doors
+  end
+
+  def about
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'site/index'
+
+  get 'site/doors'
+
+  get 'site/about'
+
+  get 'site/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'site#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
