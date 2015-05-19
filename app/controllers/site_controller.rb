4a7b8cb5b124f858efedb058d8147a05a8575962
Kamil Przybylski <supermazena@gmail.com>
2015-05-19 14:38:50 +0200

    Update Gemfile.lock for Heroku

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,13 +1,17 @@
 class SiteController < ApplicationController
   def index
+    @title = "Home"
   end
 
   def doors
+    @title = "Drzwi"
   end
 
   def about
+    @title = "O nas"
   end
 
   def help
+    @title = "Pomoc"
   end
 end
