609098175a7a987f7b4bf54d307b3526f7c85f4d
Kamil Przybylski <supermazena@gmail.com>
2015-10-18 09:13:07 +0200

    added styling

--- app/assets/javascripts/doors.js
+++ app/assets/javascripts/doors.js
@@ -17,3 +17,48 @@ jQuery(function($) {
     // $('#cancel_edit_door').show();
   });
 })
+
+// gallery
+jQuery(function($) { 
+
+  // settings
+  var $slider = $('.slider'); 
+  var $slide = 'li'; 
+  var $transition_time = 1000; 
+  var $time_between_slides = 2000;
+
+  function slides(){
+    return $slider.find($slide);
+  }
+
+  slides().fadeOut();
+
+  // set active classes
+  slides().first().addClass('active');
+  slides().first().fadeIn($transition_time);
+
+  $("#next").click(function(){
+      var $i = $slider.find($slide + '.active').index();
+
+      slides().eq($i).removeClass('active');
+      slides().eq($i).fadeOut($transition_time);
+
+      if (slides().length == $i + 1) $i = -1; // loop to start
+
+      slides().eq($i + 1).fadeIn($transition_time);
+      slides().eq($i + 1).addClass('active');
+    });
+
+    $("#prev").click(function(){
+      var $i = $slider.find($slide + '.active').index();
+
+      slides().eq($i).removeClass('active');
+      slides().eq($i).fadeOut($transition_time);
+
+      if (slides().length == $i + 1) $i = -1; // loop to start
+
+      slides().eq($i - 1).fadeIn($transition_time);
+      slides().eq($i - 1).addClass('active');
+    });
+
+});
