7fb8e2c63a58b6bec7b2517f44671734c706dd09
Kamil Przybylski <supermazena@gmail.com>
2015-12-04 10:56:06 +0100

    Gallery shows images from imgur API

--- app/controllers/doors_controller.rb
+++ app/controllers/doors_controller.rb
@@ -105,7 +105,7 @@ class DoorsController < ApplicationController
   		info = "<div><b>#{door.address}</b><br/>Super drzwi<br/><a href='/doors/#{door.id}'>Zobacz więcej</a></div><div align='right'><img src='#{door.image}' width='70' height='80'/></div>"
   	end
 
-    def imgur    
+    def imgur#(album_id)     
       # puts "Let's get some pics"
 
       headers    = {
@@ -113,7 +113,7 @@ class DoorsController < ApplicationController
       }
 
       #http       = Net::HTTP.new("https://api.imgur.com")
-      path       = "/3/album/s8YMe.json"
+      path       = "/3/album/#{@door.image}.json"
       uri = URI.parse("https://api.imgur.com"+path)
       request, data = Net::HTTP::Get.new(path, headers)
       http = Net::HTTP.new(uri.host, uri.port)
@@ -125,6 +125,6 @@ class DoorsController < ApplicationController
       # parsed["data"]["images"].each do |img|
       #   puts "#{img['datetime']}, #{img['title']}, #{img['description']}"
       # end
-      parsed["data"]["images"][1]["id"]  #alternatywne wybieranie z tablicy elementów
+ #     parsed["data"]["images"][0]["id"]  #alternatywne wybieranie z tablicy elementów
     end
 end
